0825c2744f65ca08d649b7a2f91dffd3dc5b0e3a
Stephy1995 <stephymariam56@gmail.com>
2021-11-27 18:40:04 +0530

    initial

---
+++ js/todo.js
@@ -0,0 +1,20 @@
+// Validation and load page - JS callback function
+
+function alertmsg(load) {
+
+  window.location.href = "main.html";
+}
+function validate(callBack) {
+    var user = document.getElementById("userid").value;
+    var pass = document.getElementById("pass").value;
+    if (user == "admin" && pass == "12345"){
+    
+      }
+      else {
+        alert("Incorrect Username or Password");
+        callBack();
+    }
+}
+validate(callBack, load);
+
+
